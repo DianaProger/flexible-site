02ccfd60f48d97f0d5ec7ce0aa2f527399fd4b32
DianaProger <biana.sheveleva@gmail.com>
2025-12-02 04:26:53 +0300

    Add files via upload

---
+++ html/js/script.js
@@ -0,0 +1,31 @@
+
+
+
+async function getResponce() {
+    let responce = await fetch("../js/data.json")
+    let content = await responce.text()
+    content = JSON.parse(content)
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    node_for_insert.innerHTML = ''
+
+    content.forEach((item, key) => {
+        node_for_insert.innerHTML += `
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+            <div class="card h-100 d-flex flex-column">
+                <img class="card-img-top responsive-img mt-2" 
+                     src="${item.img}" 
+                     style="height: 200px; object-fit: contain;">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text"><strong>Цена: ${item.number}</strong></p>
+                    <input type="hidden" name="vendor_code" value="${item.vendor_code}">
+                    <div class="mt-auto">
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    })
+}
+getResponce()
